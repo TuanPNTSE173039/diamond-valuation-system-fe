ffccae9a893e74920e36c2462972bd8ea68b45bf
Tuan Pham <tuanpntse173039@fpt.edu.vn>
2024-05-18 00:19:37 +0700

    added tailwind css to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <div>Hello world</div>;
+  //vertical center
+  return (
+    <>
+      <section className="min-h-screen bg-amber-100 flex flex-col justify-center items-center">
+        <div className="text-center text-2xl text-red-600 font-bold">
+          Bạn đang ở trong Develop branch
+        </div>
+        <p className="text-3xl font-extrabold text-center text-blue-500">
+          Chuyển ngay sang branch của bạn hoặc tạo branch mới
+        </p>
+        <div className="text-center text-2xl text-red-600 font-bold">
+          Bất cứ hành vi nào ở trong branch này đều sẽ gây nên tội lỗi nghiêm
+          trọng cho dự án
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
