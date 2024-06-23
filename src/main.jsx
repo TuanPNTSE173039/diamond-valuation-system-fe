f3a0369c24680a1a1318ff343a1bdfb55e50ba75
Tuan Pham <tuanpntse173039@fpt.edu.vn>
2024-06-23 18:54:31 +0700

    fix pdfFonts

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+import { pdfMake as pdfFonts } from "pdfmake/build/vfs_fonts";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -10,7 +10,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store.js";
 
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+pdfMake.vfs = pdfFonts.vfs;
 pdfMake.tableLayouts = {
   receiptLayout: {
     hLineWidth: function (i, node) {
