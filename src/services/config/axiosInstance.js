c438ef620b18d869cafccb80c2f4e02abd4c532d
Tuan Pham <tuanpntse173039@fpt.edu.vn>
2024-06-24 23:21:38 +0700

    change basedURL for localhost

--- src/services/config/axiosInstance.js
+++ src/services/config/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import AuthService from "../auth.service.js";
 
 const axiosInstance = axios.create({
-  baseURL: "https://diamond-valuation-system.onrender.com/api/v1/",
-  // baseURL: "http://localhost:8080/api/v1/",
+  // baseURL: "https://diamond-valuation-system.onrender.com/api/v1/",
+  baseURL: "http://localhost:8080/api/v1/",
   timeout: 50000,
   headers: {
     "Content-Type": "application/json",
